3d041c8b7a1f43077650aa50a4f44ea49be5395e
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2025-11-01 15:07:55 +0100

    Include example dictation app

--- pebbenote/src/c/pebbenote.c
+++ pebbenote/src/c/pebbenote.c
@@ -1,60 +1,74 @@
 #include <pebble.h>
 
-static Window *s_window;
-static TextLayer *s_text_layer;
+static Window *s_main_window;
+static TextLayer *s_output_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+static DictationSession *s_dictation_session;
+static char s_last_text[512];
+
+/******************************* Dictation API ********************************/
+
+static void dictation_session_callback(DictationSession *session, DictationSessionStatus status, 
+                                       char *transcription, void *context) {
+  if(status == DictationSessionStatusSuccess) {
+    // Display the dictated text
+    snprintf(s_last_text, sizeof(s_last_text), "Transcription:\n\n%s", transcription);
+    text_layer_set_text(s_output_layer, s_last_text);
+  } else {
+    // Display the reason for any error
+    static char s_failed_buff[128];
+    snprintf(s_failed_buff, sizeof(s_failed_buff), "Transcription failed.\n\nError ID:\n%d", (int)status);
+    text_layer_set_text(s_output_layer, s_failed_buff);
+  }
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
+/************************************ App *************************************/
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  // Start voice dictation UI
+  dictation_session_start(s_dictation_session);
 }
 
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
-}
-
-static void prv_window_load(Window *window) {
+static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  s_output_layer = text_layer_create(GRect(bounds.origin.x, (bounds.size.h - 24) / 2, bounds.size.w, bounds.size.h));
+  text_layer_set_text(s_output_layer, "Press Select to get input!");
+  text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
 }
 
-static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+static void window_unload(Window *window) {
+  text_layer_destroy(s_output_layer);
 }
 
-static void prv_init(void) {
-  s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
-  window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
+static void init() {
+  s_main_window = window_create();
+  window_set_click_config_provider(s_main_window, click_config_provider);
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
   });
-  const bool animated = true;
-  window_stack_push(s_window, animated);
+  window_stack_push(s_main_window, true);
+
+  // Create new dictation session
+  s_dictation_session = dictation_session_create(sizeof(s_last_text), dictation_session_callback, NULL);
 }
 
-static void prv_deinit(void) {
-  window_destroy(s_window);
+static void deinit() {
+  // Free the last session data
+  dictation_session_destroy(s_dictation_session);
+
+  window_destroy(s_main_window);
 }
 
-int main(void) {
-  prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
+int main() {
+  init();
   app_event_loop();
-  prv_deinit();
+  deinit();
 }
